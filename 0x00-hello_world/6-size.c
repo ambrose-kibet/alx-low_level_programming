5dc6a0eb24497f3a2d33ef66dcae8eabb261e62a
ambrose-kibet <ambrosekibet36@gmail.com>
2022-06-02 09:34:16 +0000

    first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+
+	printf("Size of a char: %d\n byte(s)", sizeof(char));
+	printf("Size of a int: %d\n byte(s)", sizeof(int));
+	printf("Size of a long int: %d\n byte(s)", sizeof(long int));
+	printf("Size of a long long int: %d\n byte(s)", sizeof(long long int));
+	printf("Size of a float: %d\n byte(s)", sizeof(float));
+	return (0);
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+this is a alow-level programming readme
