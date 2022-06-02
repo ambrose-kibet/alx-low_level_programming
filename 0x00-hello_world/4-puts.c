81397195418dbb17ee202a8b5f35ea192a326d82
ambrose-kibet <ambrosekibet36@gmail.com>
2022-06-02 12:05:25 +0000

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+
 /**
- * main - prints exactly "and that piece of art is useful"
-*/
+ * main - prints exactly
+ * Programming is like building a multilingual puzzle
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
